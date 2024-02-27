531d43d778522096d32ce9d317c89d0345d3de29
Shahanawaz <shahanawazkhan579@gmail.com>
2024-02-27 12:45:58 +0530

    removed errors

--- src/app/(blog)/blog3/[slug]/page.js
+++ src/app/(blog)/blog3/[slug]/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 // import { blogData } from "@/components/blog1/blogData"
 // import BlogLists from "@/components/blog1/BlogLists"
 import UserProfile from "@/components/blog1/UserProfile";
@@ -7,36 +7,34 @@ import FeaturedPosts from "@/components/blog1/FeaturedPosts";
 import Blog from "@/components/blog3/Blog";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 export default function SlugPage({ params }) {
-
-  const { slug } = params
+  const { slug } = params;
 
   const [blogData, setBlogData] = useState({});
 
-  const handleGetBlog = async (e) => {
+  const handleGetBlog = useCallback(async () => {
     try {
-      
       const response = await fetch(`/api/getblog?slug=${slug}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-        });
+      });
 
       const { error, result } = await response.json();
 
-      setBlogData(result)
+      setBlogData(result);
 
       if (error !== undefined) {
-        console.log('Blog Get error:', error);
+        console.log("Blog Get error:", error);
       }
     } catch (error) {
-      console.error('Blog Get operation error', error);
+      console.error("Blog Get operation error", error);
     }
-  };
+  }, [slug]);
 
   useEffect(() => {
     async function fetchData() {
@@ -49,19 +47,17 @@ export default function SlugPage({ params }) {
       }
     }
     fetchData();
-  }, [slug]); // Add slug as dependency to rerun effect when slug changes
+  }, [handleGetBlog, slug]); // Add slug as dependency to rerun effect when slug changes
 
   return (
-    <div style={{ overflowY: "scroll", scrollbarColor: "white white", scrollbarWidth: 'thin', height: '100vh' }}>
+    <div style={{ overflowY: "scroll", scrollbarColor: "white white", scrollbarWidth: "thin", height: "100vh" }}>
       <header>
         <Navbar />
       </header>
       <main>
         <div className="mx-auto max-w-2xl px-6 py-10 sm:px-8 sm:py-16 lg:max-w-7xl ">
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-10 sm:gap-x-10">
-            <div className="col-span-8">
-              {blogData && <Blog blog={blogData} />}
-            </div>
+            <div className="col-span-8">{blogData && <Blog blog={blogData} />}</div>
 
             <div className=" col-span-4 space-y-10">
               <UserProfile />
--- src/components/blog3/Blog.js
+++ src/components/blog3/Blog.js
@@ -1,35 +1,31 @@
-"use client"
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Blog({ blog }) {
+  return (
+    <div className="space-y-10">
+      {
+        <div className="space-y-4 ">
+          <div className="card-zoom bg-gray-100 w-full h-[28vh] sm:h-[60vh] rounded-xl ">
+            <button className="absolute z-10 top-4 end-4 bg-indigo-500 hover:bg-indigo-700 text-white hover:text-gray-200 shadow-2xl hover:shadow-none font-semibold p-2 rounded-full "></button>
+            <div className="card-zoom-image">
+              <Link href="/">
+                <Image src={blog.image} alt="blogImage" width={1000} height={1000} className="h-[100%]" />
+              </Link>
+            </div>
+          </div>
 
-    return (
-        <div className="space-y-10">
-            {
-                <div className="space-y-4 ">
-                    <div className="card-zoom bg-gray-100 w-full h-[28vh] sm:h-[60vh] rounded-xl ">
-                        <button className="absolute z-10 top-4 end-4 bg-indigo-500 hover:bg-indigo-700 text-white hover:text-gray-200 shadow-2xl hover:shadow-none font-semibold p-2 rounded-full " >
-                            </button>
-                        <div className="card-zoom-image">
-                            <Link href='/' >
-                                <Image src={blog.image} width={1000} height={1000} className="h-[100%]" />
-                            </Link>
-                        </div>
-                    </div>
+          <div className="flex flex-col justify-center items-center space-y-6 pb-6">
+            <Link href="/">
+              <h1 className="text-gray-800 hover:text-red-600 hover:underline text-2xl font-bold">{blog.title}</h1>
+            </Link>
 
-                    <div className="flex flex-col justify-center items-center space-y-6 pb-6">
-                        
-                        <Link href='/'>
-                            <h1 className="text-gray-800 hover:text-red-600 hover:underline text-2xl font-bold">{blog.title}</h1>
-                        </Link>
-                        
-                        <p className="text-justify text-gray-600 text-base font-normal leading-8" dangerouslySetInnerHTML={{ __html: blog.content }}></p>
-                        {/* <p className="text-justify text-gray-600 text-base font-normal leading-8">{blog.content}</p> */}
-                    </div>
-                </div>
-
-            }
+            <p className="text-justify text-gray-600 text-base font-normal leading-8" dangerouslySetInnerHTML={{ __html: blog.content }}></p>
+            {/* <p className="text-justify text-gray-600 text-base font-normal leading-8">{blog.content}</p> */}
+          </div>
         </div>
-    )
+      }
+    </div>
+  );
 }
